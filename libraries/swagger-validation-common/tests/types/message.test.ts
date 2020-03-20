5fb682ca4c37c4325bc7c4f2ce424a1592e765d1
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2020-03-20 13:04:11 +0800

    add message definition

---
+++ libraries/swagger-validation-common/tests/types/message.test.ts
@@ -0,0 +1,19 @@
+import { Message } from "../../src/types/message";
+import rawMessage from "./rawMessage.json";
+import resultMessage from "./resultMessage.json";
+
+describe("Message", () => {
+  test("should parse raw message", () => {
+    const rawMsg = JSON.stringify(rawMessage);
+    const msg: Message = JSON.parse(rawMsg);
+    expect(msg.type).toBe("Raw");
+    expect(msg).toMatchSnapshot();
+  });
+
+  test("should parse result message", () => {
+    const resultMsg = JSON.stringify(resultMessage);
+    const msg: Message = JSON.parse(resultMsg);
+    expect(msg.type).toBe("Result");
+    expect(msg).toMatchSnapshot();
+  });
+});
